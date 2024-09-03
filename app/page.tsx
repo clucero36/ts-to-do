ec54b77c7ff30dc1feb2dbeaf257b352edae95f1
llucero <clucero3696@gmail.com>
2024-09-03 14:30:30 -0700

    TS Practice

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,60 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+
+import { useState } from "react";
+
+let id = 0;
+const STARTING_TASKS = [
+  {id: id++, taskLabel: 'Wash the dishes.'},
+  {id: id++, taskLabel: 'Clean the room.'},
+  {id: id++, taskLabel: 'Walk the dog.'}
+]
 
 export default function Home() {
+
+  const [taskList, setTaskList] = useState(STARTING_TASKS);
+  const [newTask, SetNewTask] = useState('');
+
+  const addTask = () => {
+    setTaskList([...taskList].concat({
+      id: id++,
+      taskLabel: newTask
+    }))
+  }
+
+  const deleteTask = (delete_id: number) => {
+    setTaskList(taskList.filter((task) => task.id !== delete_id))
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div>
+      <div className="input">
+        <input 
+          type='text'
+          placeholder='Add To Your List'
+          aria-label='Add To Your List'
+          value={newTask}
+          onChange={(e) => SetNewTask(e.target.value)}
         />
+        <button onClick={addTask}>
+          Submit
+        </button>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="list">
+        {
+          taskList.map(({taskLabel, id}) => {
+            return (
+              <ul key={taskLabel}>
+                <li>
+                  {`${taskLabel} ${id}`}
+                  <button onClick={() => deleteTask(id)}>
+                    Delete
+                  </button>
+                </li>
+              </ul>
+            )
+          })
+        }
       </div>
-    </main>
-  );
+    </div>
+  )
 }
